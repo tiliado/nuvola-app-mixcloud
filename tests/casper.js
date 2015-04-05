9c0ddf073029f6c837e33f91125da232a78d9608
Samuel Mansour <nuvola-app-mixcloud@yay.ovh>
2015-04-06 01:16:40 +0200

    updated tests

--- tests/casper.js
+++ tests/casper.js
@@ -1,47 +1,8 @@
-var Mixcloud = {
-  "scopes": null,
-  "timeout": null
-};
-
-//Mixcloud.ready = function() {
-//  return window.angular != null;
-//};
-
-//Mixcloud.init = function() {
-//  this.waitFor(Mixcloud.ready, Mixcloud.loaded, Mixcloud.retry);
-//};
-//
-//Mixcloud.setup = function() {
-//  Mixcloud.scopes.global = $(document.body).scope();
-//  Mixcloud.scopes.PlayerQueueCtrl = $(
-//          document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]')).scope();
-//  
-//  require('utils').dump(Mixcloud);
-//};
-//
-//Mixcloud.retry = function(){
-//  console.info("Angular not ready");
-//};
-
-var conf = {
-  viewportSize: {
-    width: 1024,
-    height: 768
-  },
-  pageSettings: {
-    "userAgent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.10 (KHTML, like Gecko) Chrome/23.0.1262.0 Safari/537.10',
-    "loadImages": false,
-    "loadPlugins": false,
-    "webSecurityEnabled": false,
-    "ignoreSslErrors": true
-  }
-};
-
 var testCount = 1;
-conf.verbose = true;
-conf.logLevel = "debug";
 
-var casper = require('casper').create(conf);
+//casper.options.clientScripts = ["integrate.js"];
+casper.options.verbose = true;
+casper.options.logLevel = "debug";
 
 casper.on('page.resource.requested', function(requestData, request) {
   if (requestData.url.indexOf('mixcloud.com') === -1) {
@@ -49,15 +10,41 @@ casper.on('page.resource.requested', function(requestData, request) {
   }
 });
 
-casper.start('https://www.mixcloud.com', Mixcloud.init);
-
 casper.test.begin("Testing Mixcloud", testCount, function redditTest(test) {
   casper.start("https://www.mixcloud.com");
+
   casper.then(function() {
+    this.log(typeof nuvola, "warning");    
+  });
+
+//  casper.then(function(){
+//    test.assertEval(function() {
+//      casper.log(typeof nuvola, "debug");
+//      return typeof nuvola != "undefined";
+//    }, "nuvola");
+//  });
+//  
+//  
+//  casper.then(function() {
 //    test.assertEval(function() {
 //      return typeof $ != "undefined";
-//    });
-  });
+//    }, "Angular");
+//    
+//    test.assertEval(function(){
+//      var obj = $ && $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'));
+//      return obj && obj != null && typeof obj.scope() == "object";
+//    }, "Angular global scope");
+//    
+//    test.assertEval(function(){
+//      var obj = $ && $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'));
+//      return  obj && obj != null && typeof obj.scope() == "object";
+//    }, "Angular player scope");
+//    
+//    test.assertEval(function(){
+//      var obj = $ && $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'));
+//      return  obj && obj != null && typeof obj.scope() == "object" && obj.scope().queue.cloudcastList;
+//    }, "Angular player scope : playlist");
+//  });
 
   casper.run(function() {
     test.done();
