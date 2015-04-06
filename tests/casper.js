3226749aded387ec225e949f0eb8f6f437a703e5
Samuel Mansour <nuvola-app-mixcloud@yay.ovh>
2015-04-07 01:04:54 +0200

    update tests

--- tests/casper.js
+++ tests/casper.js
@@ -1,50 +1,39 @@
 var testCount = 1;
 
-//casper.options.clientScripts = ["integrate.js"];
+//casper.options.clientScripts = ["tests/mockup.js", "integrate.js"];
 casper.options.verbose = true;
 casper.options.logLevel = "debug";
-
-casper.on('page.resource.requested', function(requestData, request) {
-  if (requestData.url.indexOf('mixcloud.com') === -1) {
-    request.abort();
-  }
-});
+casper.options.pageSettings.loadImages = false;
+casper.options.loadPlugins = false;
 
 casper.test.begin("Testing Mixcloud", testCount, function redditTest(test) {
   casper.start("https://www.mixcloud.com");
 
   casper.then(function() {
-    this.log(typeof nuvola, "warning");    
+    test.assertEval(function() {
+      return typeof $ != "undefined";
+    }, "Angular");
+    
+    test.assertEval(function(){
+      var obj = $ && $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'));
+      return obj && obj != null && typeof obj.scope() == "object";
+    }, "Angular global scope");
+    
+    test.assertEval(function(){
+      var obj = $ && $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'));
+      return  obj && obj != null && typeof obj.scope() == "object";
+    }, "Angular player scope");
+    
+    test.assertEval(function(){
+      var obj = $ && $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'));
+      return  obj && obj != null && typeof obj.scope() == "object" && obj.scope().queue.cloudcastList;
+    }, "Angular player scope : playlist");
   });
-
-//  casper.then(function(){
-//    test.assertEval(function() {
-//      casper.log(typeof nuvola, "debug");
-//      return typeof nuvola != "undefined";
-//    }, "nuvola");
-//  });
-//  
-//  
-//  casper.then(function() {
-//    test.assertEval(function() {
-//      return typeof $ != "undefined";
-//    }, "Angular");
-//    
-//    test.assertEval(function(){
-//      var obj = $ && $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'));
-//      return obj && obj != null && typeof obj.scope() == "object";
-//    }, "Angular global scope");
-//    
-//    test.assertEval(function(){
-//      var obj = $ && $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'));
-//      return  obj && obj != null && typeof obj.scope() == "object";
-//    }, "Angular player scope");
-//    
-//    test.assertEval(function(){
-//      var obj = $ && $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'));
-//      return  obj && obj != null && typeof obj.scope() == "object" && obj.scope().queue.cloudcastList;
-//    }, "Angular player scope : playlist");
-//  });
+  
+  casper.then(function(){
+    Casper.clickLabel('Play Chart');
+    casper.waitForSelector('').success();
+  });
 
   casper.run(function() {
     test.done();
---
+++ tests/mockup.js
@@ -0,0 +1,8 @@
+var emptyObj = {};
+var emptyVal = null;
+var emptyFunc = function() {};
+
+var Nuvola = emptyObj;
+Nuvola.$object = emptyFunc;
+Nuvola.MediaPlayer = emptyVal;
+casper.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy');
