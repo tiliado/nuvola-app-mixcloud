3285d12db8647f85478642fab839763b21739e71
Samuel Mansour <nuvola-app-mixcloud@yay.ovh>
2015-04-10 01:04:53 +0200

    additional test cases

--- tests/casper.js
+++ tests/casper.js
@@ -1,6 +1,8 @@
-var testCount = 3;
-var title;
+var testCount = 7;
+var title, testCallback;
+var scope = {};
 
+// configs
 casper.options.verbose = true;
 casper.options.logLevel = "debug";
 casper.options.loadImages = false;
@@ -10,52 +12,57 @@ casper.options.viewportSize = {
   height: 1200
 };
 
+// tests
 casper.test.begin("Testing Mixcloud", testCount, function mixcloudTest(test) {
   casper.start("https://www.mixcloud.com");
 
   casper.thenClick('.button[m-play-all-button]', function playAllClick() {
-    casper.waitForSelector('div.player-wrapper:not(.ng-hide)', function afterPlayAll() {
+    casper.waitUntilVisible('.player-cloudcast-image img', function afterPlayAll() {
 
-      var scope = {};
-
-      scope.angularType = casper.evaluate(function() {
-        return typeof angular;
+      scope.angular = casper.evaluate(function() {
+        return typeof angular != "undefined" && typeof $ != "undefined";
       });
+
+      title = "Angular is detected";
+      test.assert(scope.angular, title);
 
       scope.global = casper.evaluate(function() {
         return $(__utils__.findOne('body')).scope();
       });
 
-      scope.player = casper.evaluate(function() {
-        return $(__utils__.findOne('.ng-scope[ng-controller="PlayerQueueCtrl"]')).scope();
-      });
-
-      title = "Angular";
-      test.assertNotEquals(scope.angularType, "undefined", title);
-
-      title = "Angular global scope";
+      title = "Angular scope : global";
       test.assertEquals(typeof scope.global.$id == "string" && scope.global.$id.length > 0, true,
               title);
 
-      title = "Angular player scope";
-      test.assertEquals(typeof scope.player.$id == "string" && scope.player.$id.length > 0, true,
-              title);
-      
-      title = "Angular playlist";
       scope.player = casper.evaluate(function() {
         return $(__utils__.findOne('.ng-scope[ng-controller="PlayerQueueCtrl"]')).scope();
       });
-      
-      casper.log(scope.player);
-      
-//      test.assertEval(function() {
-//        var scope = $(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]')).scope();
-//        casper.log("ok","warning");
-//        return true;
-////        return typeof utils.getPropertyPath(scope, 'playerQueue.queue.cloudcastList.length') != "undefined";
-//      }, title);
 
-//      casper.log(document.querySelector('.ng-scope[ng-controller="PlayerQueueCtrl"]'), "debug");
+      title = "Angular scope: player";
+      test.assert(typeof scope.player.$id == "string" && scope.player.$id.length > 0, title);
+
+      title = "Angular property : player.playing";
+      test.assert(typeof scope.player.player.playing == "boolean", title);
+
+      title = "Angular property : nowPlaying.currentDisplayTrack";
+      test.assert(typeof scope.player.player.nowPlaying.currentDisplayTrack != "undefined", title);
+      
+      title = "Angular property : queue.cloudcastList";
+      testCallback = function() {
+        return $(__utils__.findOne('.ng-scope[ng-controller="PlayerQueueCtrl"]'))
+        .scope().playerQueue.queue.cloudcastList.toArray().length > 0;
+      };
+      
+      test.assertEval(testCallback, title);
+
+      title = "Angular property: currentCloudcast";
+      testCallback = function() {
+        return typeof $(__utils__.findOne('.ng-scope[ng-controller="PlayerQueueCtrl"]'))
+        .scope().player.currentCloudcast != "undefined";
+      };
+      test.assertEval(testCallback, title);
+
+      require('utils').dump(scope.player.player.nowPlaying);
 
       // screenshot
       casper.capture('tests/screenshot/snapshot.png');
